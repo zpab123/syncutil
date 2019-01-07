617c41137f2bfc6b450613abce44ea91e54f882c
张鹏（金山） <287206570@qq.com>
2019-01-07 16:18:39 +0800

    优化原子代码

--- atomic_int.go
+++ atomic_int.go
@@ -17,13 +17,18 @@ func (ai *AtomicInt32) Store(v int32) {
 }
 
 // 原子性的将 delta 的值添加到 *AtomicInt32 并返回新值。
-func (ai *AtomicInt32) Add(delta int32) {
-	atomic.AddInt32((*int32)(ai), delta)
+func (ai *AtomicInt32) Add(delta int32) (new int32) {
+	return atomic.AddInt32((*int32)(ai), delta)
 }
 
 // 原子性的获取 *AtomicInt32 的值。
 func (ai *AtomicInt32) Load() int32 {
 	return atomic.LoadInt32((*int32)(ai))
+}
+
+// 原子性的将 v 的值 与 原来的值 交换，返回原来的值
+func (ai *AtomicInt32) Swap(v int32) (old int32) {
+	return atomic.SwapInt32((*int32)(ai), v)
 }
 
 // /////////////////////////////////////////////////////////////////////////////
@@ -36,13 +41,18 @@ func (ui32 *AtomicUint32) Store(v uint32) {
 }
 
 // 原子性的将 delta 的值添加到 *AtomicUint32 并返回新值。
-func (ui32 *AtomicUint32) Add(delta uint32) {
-	atomic.AddUint32((*uint32)(ui32), delta)
+func (ui32 *AtomicUint32) Add(delta uint32) (new uint32) {
+	return atomic.AddUint32((*uint32)(ui32), delta)
 }
 
 // 原子性的获取 *AtomicUint32 的值。
 func (ui32 *AtomicUint32) Load() uint32 {
 	return atomic.LoadUint32((*uint32)(ui32))
+}
+
+// 原子性的将 v 的值 与 原来的值 交换，返回原来的值
+func (ui32 *AtomicUint32) Swap(v uint32) (old uint32) {
+	return atomic.SwapUint32((*uint32)(ui32), v)
 }
 
 // /////////////////////////////////////////////////////////////////////////////
@@ -78,8 +88,8 @@ func (ai *AtomicInt64) Store(v int64) {
 }
 
 // 原子性的将 delta 的值添加到 *AtomicInt64 并返回新值。
-func (ai *AtomicInt64) Add(delta int64) {
-	atomic.AddInt64((*int64)(ai), delta)
+func (ai *AtomicInt64) Add(delta int64) (new int64) {
+	return atomic.AddInt64((*int64)(ai), delta)
 }
 
 // 原子性的获取 *AtomicInt64 的值。
@@ -96,18 +106,34 @@ func (ai *AtomicInt64) Swap(v int64) (old int64) {
 // bool 原子变量
 
 // bool 类型的 原子变量
-type AtomicBool AtomicInt
+type AtomicBool AtomicInt32
 
 // 原子性的将 v 的值保存到 *AtomicBool
 func (ab *AtomicBool) Store(v bool) {
 	if v {
-		(*AtomicInt)(ab).Store(1)
+		(*AtomicInt32)(ab).Store(1)
 	} else {
-		(*AtomicInt)(ab).Store(0)
+		(*AtomicInt32)(ab).Store(0)
 	}
 }
 
 // 原子性的获取 *AtomicBool 的值。
 func (ab *AtomicBool) Load() bool {
-	return (*AtomicInt)(ab).Load() != 0
+	return (*AtomicInt32)(ab).Load() != 0
+}
+
+// 将 v 与 原来的值交换，并返回原来的值
+func (ab *AtomicBool) Swap(v bool) bool {
+	// 原来的值
+	old := ((*AtomicInt32)(ab).Load() != 0)
+
+	// 交换
+	if v {
+		(*AtomicInt32)(ab).Store(0)
+	} else {
+		(*AtomicInt32)(ab).Store(1)
+	}
+
+	// 返回原来的值
+	return old
 }
