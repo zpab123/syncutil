4c31bd672922bd8f6361c2328bbdce1c256d85dc
zp_amd <287206570@qq.com>
2019-01-27 21:55:17 +0800

    增加比较交换API

--- atomic_int.go
+++ atomic_int.go
@@ -11,69 +11,78 @@ import "sync/atomic"
 // int32 类型的 原子变量
 type AtomicInt32 int32
 
+// 原子性的获取 *AtomicInt32 的值。
+func (this *AtomicInt32) Load() int32 {
+	self := (*int32)(this)
+
+	return atomic.LoadInt32(self)
+}
+
 // 原子性的将 v 的值保存到 *AtomicInt32
-func (ai *AtomicInt32) Store(v int32) {
-	atomic.StoreInt32((*int32)(ai), v)
+func (this *AtomicInt32) Store(v int32) {
+	self := (*int32)(this)
+
+	atomic.StoreInt32(self, v)
 }
 
 // 原子性的将 delta 的值添加到 *AtomicInt32 并返回新值。
-func (ai *AtomicInt32) Add(delta int32) (new int32) {
-	return atomic.AddInt32((*int32)(ai), delta)
-}
+func (this *AtomicInt32) Add(delta int32) int32 {
+	self := (*int32)(this)
 
-// 原子性的获取 *AtomicInt32 的值。
-func (ai *AtomicInt32) Load() int32 {
-	return atomic.LoadInt32((*int32)(ai))
+	return atomic.AddInt32(self, delta)
 }
 
 // 原子性的将 v 的值 与 原来的值 交换，返回原来的值
-func (ai *AtomicInt32) Swap(v int32) (old int32) {
-	return atomic.SwapInt32((*int32)(ai), v)
+func (this *AtomicInt32) Swap(v int32) int32 {
+	self := (*int32)(this)
+
+	return atomic.SwapInt32(self, v)
+}
+
+// 将 AtomicInt32 的值与 old 进行比较，如果相等。则将 AtomicInt32 的值替换为 newv，并返回 true。否则返回 false
+func (this *AtomicInt32) CompareAndSwap(old int32, newv int32) bool {
+	self := (*int32)(this)
+
+	return atomic.CompareAndSwapInt32(self, old, newv)
 }
 
 // /////////////////////////////////////////////////////////////////////////////
 // Uint32 原子变量
 type AtomicUint32 uint32
 
+// 原子性的获取 *AtomicUint32 的值。
+func (this *AtomicUint32) Load() uint32 {
+	self := (*uint32)(this)
+
+	return atomic.LoadUint32(self)
+}
+
 // 原子性的将 v 的值保存到 *AtomicUint32
-func (ui32 *AtomicUint32) Store(v uint32) {
-	atomic.StoreUint32((*uint32)(ui32), v)
+func (this *AtomicUint32) Store(v uint32) {
+	self := (*uint32)(this)
+
+	atomic.StoreUint32(self, v)
 }
 
 // 原子性的将 delta 的值添加到 *AtomicUint32 并返回新值。
-func (ui32 *AtomicUint32) Add(delta uint32) (new uint32) {
-	return atomic.AddUint32((*uint32)(ui32), delta)
-}
+func (this *AtomicUint32) Add(delta uint32) uint32 {
+	self := (*uint32)(this)
 
-// 原子性的获取 *AtomicUint32 的值。
-func (ui32 *AtomicUint32) Load() uint32 {
-	return atomic.LoadUint32((*uint32)(ui32))
+	return atomic.AddUint32(self, delta)
 }
 
 // 原子性的将 v 的值 与 原来的值 交换，返回原来的值
-func (ui32 *AtomicUint32) Swap(v uint32) (old uint32) {
-	return atomic.SwapUint32((*uint32)(ui32), v)
+func (this *AtomicUint32) Swap(v uint32) uint32 {
+	self := (*uint32)(this)
+
+	return atomic.SwapUint32(self, v)
 }
 
-// /////////////////////////////////////////////////////////////////////////////
-// int 原子变量
+// 将 AtomicUint32 的值与 old 进行比较，如果相等。则将 AtomicUint32 的值替换为 newv，并返回 true。否则返回 false
+func (this *AtomicUint32) CompareAndSwap(old uint32, newv uint32) bool {
+	self := (*uint32)(this)
 
-// int 类型的 原子变量 （用 int64 表示的）
-type AtomicInt int64
-
-// 原子性的将 v 的值保存到 *AtomicInt
-func (ai *AtomicInt) Store(v int) {
-	atomic.StoreInt64((*int64)(ai), int64(v))
-}
-
-// 原子性的将 delta 的值添加到 *AtomicInt 并返回新值。
-func (ai *AtomicInt) Add(delta int) {
-	atomic.AddInt64((*int64)(ai), int64(delta))
-}
-
-// 原子性的获取 *AtomicInt 的值。
-func (ai *AtomicInt) Load() int {
-	return int(atomic.LoadInt64((*int64)(ai)))
+	return atomic.CompareAndSwapUint32(self, old, newv)
 }
 
 // /////////////////////////////////////////////////////////////////////////////
@@ -82,24 +91,60 @@ func (ai *AtomicInt) Load() int {
 // int64 类型的 原子变量
 type AtomicInt64 int64
 
+// 原子性的获取 *AtomicInt64 的值。
+func (this *AtomicInt64) Load() int64 {
+	self := (*int64)(this)
+
+	return atomic.LoadInt64(self)
+}
+
 // 原子性的将 v 的值保存到 *AtomicInt64
-func (ai *AtomicInt64) Store(v int64) {
-	atomic.StoreInt64((*int64)(ai), v)
+func (this *AtomicInt64) Store(v int64) {
+	self := (*int64)(this)
+
+	atomic.StoreInt64(self, v)
 }
 
 // 原子性的将 delta 的值添加到 *AtomicInt64 并返回新值。
-func (ai *AtomicInt64) Add(delta int64) (new int64) {
-	return atomic.AddInt64((*int64)(ai), delta)
-}
+func (this *AtomicInt64) Add(delta int64) int64 {
+	self := (*int64)(this)
 
-// 原子性的获取 *AtomicInt64 的值。
-func (ai *AtomicInt64) Load() int64 {
-	return atomic.LoadInt64((*int64)(ai))
+	return atomic.AddInt64(self, delta)
 }
 
 // 将 v 与 原来的值交换，并返回原来的值
-func (ai *AtomicInt64) Swap(v int64) (old int64) {
-	return atomic.SwapInt64((*int64)(ai), v)
+func (this *AtomicInt64) Swap(v int64) int64 {
+	self := (*int64)(this)
+
+	return atomic.SwapInt64(self, v)
+}
+
+// 将 AtomicInt64 的值与 old 进行比较，如果相等。则将 AtomicInt64 的值替换为 newv，并返回 true。否则返回 false
+func (this *AtomicInt64) CompareAndSwap(old int64, newv int64) bool {
+	self := (*int64)(this)
+
+	return atomic.CompareAndSwapInt64(self, old, newv)
+}
+
+// /////////////////////////////////////////////////////////////////////////////
+// int 原子变量
+
+// int 类型的 原子变量 （用 int64 表示的）
+type AtomicInt int64
+
+// 原子性的获取 *AtomicInt 的值。
+func (this *AtomicInt) Load() int {
+	return int(atomic.LoadInt64((*int64)(this)))
+}
+
+// 原子性的将 v 的值保存到 *AtomicInt
+func (this *AtomicInt) Store(v int) {
+	atomic.StoreInt64((*int64)(this), int64(v))
+}
+
+// 原子性的将 delta 的值添加到 *AtomicInt 并返回新值。
+func (this *AtomicInt) Add(delta int) {
+	atomic.AddInt64((*int64)(this), int64(delta))
 }
 
 // /////////////////////////////////////////////////////////////////////////////
